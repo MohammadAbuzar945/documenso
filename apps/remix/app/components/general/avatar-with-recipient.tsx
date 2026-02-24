178212ec46c51737807e4f8c02b18bf38840a3b0
Mohammad Abuzar <135220443+abuzar-dev@users.noreply.github.com>
2026-02-24 20:23:11 +0500

    private

--- apps/remix/app/components/general/avatar-with-recipient.tsx
+++ apps/remix/app/components/general/avatar-with-recipient.tsx
@@ -1,10 +1,15 @@
+import { msg } from '@lingui/core/macro';
+import { useLingui } from '@lingui/react';
 import type { Recipient } from '@prisma/client';
 import { DocumentStatus } from '@prisma/client';
 
+import { useCopyToClipboard } from '@documenso/lib/client-only/hooks/use-copy-to-clipboard';
 import { getRecipientType } from '@documenso/lib/client-only/recipient-type';
+import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 import { RECIPIENT_ROLES_DESCRIPTION } from '@documenso/lib/constants/recipient-roles';
 import { recipientAbbreviation } from '@documenso/lib/utils/recipient-formatter';
 import { cn } from '@documenso/ui/lib/utils';
+import { useToast } from '@documenso/ui/primitives/use-toast';
 
 import { StackAvatar } from './stack-avatar';
 
@@ -14,11 +19,34 @@ export type AvatarWithRecipientProps = {
 };
 
 export function AvatarWithRecipient({ recipient, documentStatus }: AvatarWithRecipientProps) {
+  const [, copy] = useCopyToClipboard();
+
+  const { _ } = useLingui();
+  const { toast } = useToast();
+
+  const signingToken = documentStatus === DocumentStatus.PENDING ? recipient.token : null;
+
+  const onRecipientClick = () => {
+    if (!signingToken) {
+      return;
+    }
+
+    void copy(`${NEXT_PUBLIC_WEBAPP_URL()}/sign/${signingToken}`).then(() => {
+      toast({
+        title: _(msg`Copied to clipboard`),
+        description: _(msg`The signing link has been copied to your clipboard.`),
+      });
+    });
+  };
+
   return (
     <div
       className={cn('my-1 flex items-center gap-2', {
-        'opacity-60': documentStatus === DocumentStatus.COMPLETED,
+        'cursor-pointer hover:underline': signingToken,
       })}
+      role={signingToken ? 'button' : undefined}
+      title={signingToken ? _(msg`Click to copy signing link for sending to recipient`) : undefined}
+      onClick={onRecipientClick}
     >
       <StackAvatar
         first={true}
@@ -29,6 +57,9 @@ export function AvatarWithRecipient({ recipient, documentStatus }: AvatarWithRec
 
       <div
         className="text-sm text-muted-foreground"
+        title={
+          signingToken ? _(msg`Click to copy signing link for sending to recipient`) : undefined
+        }
       >
         <p>{recipient.email || recipient.name}</p>
         <p className="text-xs text-muted-foreground/70">
